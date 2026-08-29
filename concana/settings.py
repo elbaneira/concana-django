30e4c7d42c8c0c562931ef0f4b38330fb3170bc9
ElbaN <elbaneira@gmail.com>
2026-08-29 03:23:04 -0400

    Actualizar ALLOWED_HOSTS

--- concana/settings.py
+++ concana/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-cww1xpo63#nnu1w-@=2ibl338h&ubktz_aqu)jwdvar20ptw)j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =['*']
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
